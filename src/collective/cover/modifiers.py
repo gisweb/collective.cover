8cb2e164c27f98db595a4edc4892e9c566844806
Juan Pablo Giménez <jpg@rosario.com>
2015-09-29 09:54:05 -0300

    Fix cover UI to integrate with CMFEditions Refactor modifier to remove scales on versioning Flake8, refactor to fulfill complexity

--- src/collective/cover/modifiers.py
+++ src/collective/cover/modifiers.py
@@ -54,57 +54,54 @@ class TilesCloneNamedFileBlobs:
             blob_file_classes.append(NamedBlobFile)
         return tuple(blob_file_classes)
 
-    def getReferencedAttributes(self, obj):
+    def _compareBlobs(self, tid, name, field, prior_rev):
+        file_data = {}
+        save_new = False
+        if field._blob and field.data:
+            blob_file = field.open()
+            dotted_name = '.'.join([tid, name])
+            file_data[dotted_name] = field._blob
+        if prior_rev is not None and field._blob and field.data:
+            prior_obj = prior_rev.object
+            save_new = True
+            if tid in prior_obj.list_tiles():
+                prior_tile = prior_obj.get_tile(tid)
+                prior_blob = prior_tile.data[name]
+                if prior_blob and prior_blob._blob:
+                    file_data[dotted_name] = prior_blob._blob
+                    prior_file = prior_blob.open()
+                    # Check for file size differences
+                    if (os.fstat(prior_file.fileno()).st_size ==
+                            os.fstat(blob_file.fileno()).st_size):
+                        # Files are the same size, compare line by line
+                        for line, prior_line in izip(blob_file,
+                                                     prior_file):
+                            if line != prior_line:
+                                save_new = True
+                                break
+                        else:
+                            # The files are the same, save a reference
+                            # to the prior versions blob on this
+                            # version
+                            file_data[dotted_name] = prior_blob._blob
+                            save_new = False
+        return save_new, file_data
+
+    def _getReferencedBlobs(self, obj, prior_rev):
         file_data = {}
         blob_file_classes = self._blob_file_classes()
-        # Try to get last revision, only store a new blob if the
-        # contents differ from the prior one, otherwise store a
-        # reference to the prior one.
-        # The implementation is mostly based on CMFEditions's CloneBlobs
-        # modifier.
-        repo = getToolByName(obj, 'portal_repository')
-        try:
-            prior_rev = repo.retrieve(obj)
-        except ArchivistRetrieveError:
-            prior_rev = None
-
         for tid in obj.list_tiles():
             tile = obj.get_tile(tid)
             for name, field in tile.data.items():
                 if isinstance(field, blob_file_classes):
-                    if field._blob is None or \
-                       field.data is None:
+                    if not field._blob or not field.data:
                         continue
                     blob_file = field.open()
-                    save_new = True
-                    dotted_name = '.'.join([tid, name])
-                    if prior_rev is not None:
-                        prior_obj = prior_rev.object
-                        if tid not in prior_obj.list_tiles():
-                            continue
-                        prior_tile = prior_obj.get_tile(tid)
-                        if prior_tile is None:
-                            continue
-                        prior_blob = prior_tile.data[name]
-                        if prior_blob._blob is not None:
-                            prior_file = prior_blob.open()
-
-                            # Check for file size differences
-                            if (os.fstat(prior_file.fileno()).st_size ==
-                                    os.fstat(blob_file.fileno()).st_size):
-                                # Files are the same size, compare line by line
-                                for line, prior_line in izip(blob_file,
-                                                             prior_file):
-                                    if line != prior_line:
-                                        break
-                                else:
-                                    # The files are the same, save a reference
-                                    # to the prior versions blob on this
-                                    # version
-                                    file_data[dotted_name] = prior_blob._blob
-                                    save_new = False
-
+                    save_new, data = self._compareBlobs(tid, name,
+                                                        field, prior_rev)
+                    file_data.update(data)
                     if save_new:
+                        dotted_name = '.'.join([tid, name])
                         new_blob = file_data[dotted_name] = Blob()
                         new_blob_file = new_blob.open('w')
                         try:
@@ -113,7 +110,41 @@ class TilesCloneNamedFileBlobs:
                         finally:
                             blob_file.close()
                             new_blob_file.close()
+        return file_data
 
+    def _delImageScales(self, obj, prior_rev):
+        # Remove images scales from tile,
+        # scales are regenerated on revert.
+        annotations = getattr(obj, '__annotations__', None)
+        orig_annotations = {}
+        if prior_rev:
+            getattr(prior_rev.object, '__annotations__', None)
+        blob_file_classes = self._blob_file_classes()
+        for tid in obj.list_tiles():
+            tile = obj.get_tile(tid)
+            for name, field in tile.data.items():
+                if isinstance(field, blob_file_classes):
+                    name = 'plone.tiles.scale.%s' % tid
+                    if name in annotations:
+                        del annotations[name]
+                    if name in orig_annotations:
+                        del orig_annotations[name]
+
+    def getReferencedAttributes(self, obj):
+        file_data = {}
+        # Try to get last revision, only store a new blob if the
+        # contents differ from the prior one, otherwise store a
+        # reference to the prior one.
+        # The implementation is mostly based on plone.app.versioningbehavior's
+        # CloneNamedFileBlobs modifier.
+        repo = getToolByName(obj, 'portal_repository')
+        try:
+            prior_rev = repo.retrieve(obj)
+        except ArchivistRetrieveError:
+            prior_rev = None
+
+        file_data.update(self._getReferencedBlobs(obj, prior_rev))
+        self._delImageScales(obj, prior_rev)
         return file_data
 
     def reattachReferencedAttributes(self, obj, attrs_dict):
@@ -142,7 +173,7 @@ class TilesCloneNamedFileBlobs:
                         for scale in storage.values():
                             scale_data = scale.get('data', None)
                             if isinstance(scale_data, blob_file_classes):
-                                if scale_data.data is not None:
+                                if scale_data._blob is not None:
                                     blob_refs[id(aq_base(scale_data._blob))] = True
 
         def persistent_id(obj):
